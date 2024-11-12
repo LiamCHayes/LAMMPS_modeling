f415ce92367b686a0d18399b6cd9dde5369b1b2e
Liam Hayes <liamh@scc-202.scc.bu.edu>
2024-11-12 08:35:26 -0500

    new dump

--- simple_rod/simple_rod.py
+++ simple_rod/simple_rod.py
@@ -2,11 +2,13 @@ import numpy as np
 import pandas as pd
 from lammpsWithPython.lammps_object import Simulation
 
+# dump DUMPFILE all xyz 100 dump.xyz
+
 # Init simulation
 sim = Simulation("run", 3, 0.1, 0.03, 0.1)
 
 # Beam parameters
-beam_length = 0.095
+beam_length = 0.05
 beam_stiffness = 10 ** 5
 beam_thickness = 0.003
 
@@ -17,3 +19,8 @@ viscosity = 0.000001
 
 # Add beam
 beam_type, bond_type, angle_type = sim.add_beam(50, np.array([0, 0, -beam_length/2]),np.array([0, 0, beam_length/2]), beam_thickness, beam_stiffness, density)
+
+# Run simulation
+sim.add_viscosity(viscosity)
+sim.design_dump_files(0.05)
+sim.run_simulation(1)
